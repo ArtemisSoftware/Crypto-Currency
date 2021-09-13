85d7de32eb65ecfaef6d657d1452d3f2c184a9c8
gustavo <gustavo.isaac@gmail.com>
2021-09-13 10:11:58 +0100

    Implementing API & Models

---
+++ app/src/main/java/com/artemissoftware/cryptocurrency/domain/model/CoinDetail.kt
@@ -0,0 +1,14 @@
+package com.artemissoftware.cryptocurrency.domain.model
+
+import com.artemissoftware.cryptocurrency.data.remote.dto.TeamMember
+
+data class CoinDetail(
+    val coinId: String,
+    val name: String,
+    val description: String,
+    val symbol: String,
+    val rank: Int,
+    val isActive: Boolean,
+    val tags: List<String>,
+    val team: List<TeamMember>
+)
