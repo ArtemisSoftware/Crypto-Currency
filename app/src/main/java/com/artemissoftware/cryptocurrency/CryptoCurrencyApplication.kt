fb9e7bd74ec48074ac844904f7fc3815913d1ab6
gustavo <gustavo.isaac@gmail.com>
2021-09-13 10:41:22 +0100

    Setting up dependency injection

---
+++ app/src/main/java/com/artemissoftware/cryptocurrency/CryptoCurrencyApplication.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.cryptocurrency
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CryptoCurrencyApplication: Application()
